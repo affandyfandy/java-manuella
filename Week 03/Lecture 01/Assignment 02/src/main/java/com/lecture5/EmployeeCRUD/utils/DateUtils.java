bdf96ffa9ce1350edd894aab2432cf699dd405a2
manuellaiv <13521051@std.stei.itb.ac.id>
2024-06-24 15:09:56 +0700

    feat: assignment 2

---
+++ Week 03/Lecture 01/Assignment 02/src/main/java/com/lecture5/EmployeeCRUD/utils/DateUtils.java
@@ -0,0 +1,20 @@
+package com.lecture5.EmployeeCRUD.utils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateUtils {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    public static LocalDate parseData(String dateStr){
+        try{
+            return LocalDate.parse(dateStr, formatter);
+        } catch (DateTimeParseException e){
+            return null;
+        }
+    }
+
+    public static String formatDate(LocalDate date){
+        return date.format(formatter);
+    }
+}
